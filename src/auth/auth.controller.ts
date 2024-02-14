72b0b7afe1b211f60ecd137e57abed7c673e7ff9
Ivan Yurygin <sivan2140@gmail.com>
2024-02-14 23:22:44 +0300

    authControllerUpdated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthUserDto, CreateUserDto } from 'src/users/user-dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 export class AuthController {
-	constructor(private authService: AuthService) { }
+	constructor(private authService: AuthService, private usersServise: UsersService) { }
 	
 	@Post('login')
 	async login(@Body() userDto: AuthUserDto) {
-		return this.authService.login(userDto)
+		const token = await this.authService.login(userDto)
+		const user = await this.usersServise.getUserByEmail(userDto.email)
+		return {accessToken: token.token, user: {name: user.name, email: user.email, isAuth: true}}
 	}
 
 	@Post('registration')
 	async registration(@Body() userDto: CreateUserDto) {
-		return this.authService.registration(userDto)
+		const token = await this.authService.registration(userDto)
+		return {accessToken: token.token, user: {name: userDto.name, email: userDto.email, isAuth: true}}
 	}
 }
